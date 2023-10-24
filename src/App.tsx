01984452a5fc0f64627f768711dc1c4fdef81be5
Daniele Havrylyuk <s304641@student.salfordcc.ac.uk>
2023-10-24 02:35:45 +0100

    Initial Configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import WordList from "./wordList.json";
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return WordList[Math.floor(Math.random() * WordList.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  console.log(wordToGuess);
   return <h1>Hello World!</h1>;
 }
 
